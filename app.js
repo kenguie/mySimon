82c0f9aac2e9ccc9d742834d062a93290e031254
nyhunter77 <nyhunter77@yahoo.com>
2015-02-01 00:43:07 -0500

    testing

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 angular.module('mySimon',[]) 
 
 	.controller('box1', function (){
-		this.array = ['a','b','c','d'];
-
+		this.myarray = ['a','b','c','d'];
 	});
 
 	.controller('box2', function (){
-
+		this.myarray = ['e','f','g','h'];
 	});
 
 	.controller('box3', function (){
-
+		this.myarray = ['i','j','k','l'];
 	});
 
 	.controller('box4', function (){
-
+		this.myarray = ['m','n','o','p'];
 	});
