06b9f4a63033f8054cce02325c336c79c6b2d716
nyhunter77 <nyhunter77@yahoo.com>
2015-02-01 12:29:48 -0500

    they all change color at the same time

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-angular.module('mySimon',[]) 
+var app = angular.module('mySimon',[]) 
 
-	.controller('box1', function (){
+	app.controller('box1', function (){
 		this.myarray = ['a','b','c','d'];
 	});
 
+	app.controller('box2', function (){
+		this.myarray = ['a','b','c','d'];
+	});
+
+	app.controller('box3', function (){
+		this.myarray = ['a','b','c','d'];
+	});
+
+	app.controller('box4', function (){
+		this.myarray = ['a','b','c','d'];
+	});
