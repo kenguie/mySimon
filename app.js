ebc31e2ab5e98775c3a1693a851795430768c536
nyhunter77 <nyhunter77@yahoo.com>
2015-02-01 08:16:55 -0500

    simplified / added start button

--- app.js
+++ app.js
@@ -4,14 +4,3 @@ angular.module('mySimon',[])
 		this.myarray = ['a','b','c','d'];
 	});
 
-	.controller('box2', function (){
-		this.myarray = ['e','f','g','h'];
-	});
-
-	.controller('box3', function (){
-		this.myarray = ['i','j','k','l'];
-	});
-
-	.controller('box4', function (){
-		this.myarray = ['m','n','o','p'];
-	});
