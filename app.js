5761a2e4b999fb418c777e7ebfbb6705126c3f55
nyhunter77 <nyhunter77@yahoo.com>
2015-01-31 23:28:05 -0500

    mySimon app
    Like Simon, made in Angular

---
+++ app.js
@@ -0,0 +1,18 @@
+angular.module('mySimon',[]) 
+
+	.controller('box1', function (){
+		this.array = ['a','b','c','d'];
+
+	});
+
+	.controller('box2', function (){
+
+	});
+
+	.controller('box3', function (){
+
+	});
+
+	.controller('box4', function (){
+
+	});
